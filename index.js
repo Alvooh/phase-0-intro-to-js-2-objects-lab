cda54b0210faa6b04f06b52f99ea5d58d67dad74
AlvinObala <alvinomolo99@gmail.com>
2024-04-01 23:59:59 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+const employee = {
+    name: "Alvin",
+    streetAddress: "Kasarani 2nd Avenue"
+  };
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return {...employee, [key] : value};  
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+    return {...employee, [key]: undefined};
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    delete employee[key];
+    return employee;
+}
